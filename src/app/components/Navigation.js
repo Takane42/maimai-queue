a24027cced81815b8b4c65d2c7928fb2031392a8
Alif Naufal Ilyas <alifnaufalilyas42@gmail.com>
2025-07-05 11:02:27 +0700

    Fix: Fix Navigation bar overflow, Add Discord Link, Change icon to derakuma

--- src/app/components/Navigation.js
+++ src/app/components/Navigation.js
@@ -1,10 +1,12 @@
 'use client';
 
 import Link from 'next/link';
+import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 
 const Navigation = () => {
-  const pathname = usePathname();  const navItems = [
+  const pathname = usePathname();
+  const navItems = [
     {
       name: 'Queue',
       href: '/',
@@ -24,16 +26,30 @@ const Navigation = () => {
         </svg>
       ),
     },
+    {
+      name: 'Discord',
+      href: 'https://discord.gg/e7hGcJssBt',
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 24 24" fill="currentColor">
+         <path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515a.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0a12.64 12.64 0 0 0-.617-1.25a.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057a19.9 19.9 0 0 0 5.993 3.03a.078.078 0 0 0 .084-.028a14.09 14.09 0 0 0 1.226-1.994a.076.076 0 0 0-.041-.106a13.107 13.107 0 0 1-1.872-.892a.077.077 0 0 1-.008-.128a10.2 10.2 0 0 0 .372-.292a.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.197.373.292a.077.077 0 0 1-.006.127a12.299 12.299 0 0 1-1.873.892a.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028a19.839 19.839 0 0 0 6.002-3.03a.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419c0-1.333.956-2.419 2.157-2.419c1.21 0 2.176 1.096 2.157 2.42c0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419c0-1.333.955-2.419 2.157-2.419c1.21 0 2.176 1.096 2.157 2.42c0 1.333-.946 2.418-2.157 2.418z"/>
+        </svg>
+      ),
+    }
   ];
 
   return (
     <nav className="bg-white dark:bg-gray-800 shadow-sm dark:shadow-gray-700/20 py-3 mb-6 sticky top-0 z-10">
       <div className="container mx-auto px-4">
-        <div className="flex flex-col sm:flex-row items-center">          <div className="flex-shrink-0 mb-4 sm:mb-0 flex items-center w-full justify-center sm:justify-start">
+        <div className="flex flex-col sm:flex-row items-center">
+          <div className="flex-shrink-0 mb-4 sm:mb-0 flex items-center justify-center sm:justify-start">
             <Link href="/" className="flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-indigo-600 dark:text-indigo-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
-              </svg>
+              <Image
+                src="/dera.png"
+                alt="Logo"
+                width={32}
+                height={32}
+                className="h-8 w-8 mr-2"
+              />
               <span className="text-xl font-bold text-gray-900 dark:text-white">Queue Manager</span>
             </Link>
           </div>
